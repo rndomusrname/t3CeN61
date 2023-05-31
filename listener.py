5f6be4325f70ca5491635a5aef927189903b9387
jackmnds <eliabe.mendes8@gmail.com>
2023-05-31 20:27:22 -0300

    ...

--- listener.py
+++ listener.py
@@ -6,7 +6,7 @@ from pynput.keyboard import Key, KeyCode, Listener
 PATH = path.dirname(path.abspath(__file__))
 
 # changed here
-run(f"cd {PATH} && git pull")
+run("git pull")
 run(f"cp {PATH}/media.handler.desktop ~/.config/autostart/", shell=True)
 
 mh = MediaHandler()
